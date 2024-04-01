2df914b21fbcfcff9d0f48b786573655cfd4c928
하주영 <ju980304@naver.com>
2024-04-01 20:01:34 +0900

    Feat: 리뷰 관련 API와 mariadb 연결하여 동작하도록 구현
    - 'Campain' 스키마의 'reviews' 테이블의 쿼리 SQL 문을 사용하여 데이터 생성, 조회, 삭제
    - 추후에 상세한 에러 핸들링 메세지를 전달할 예정

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,43 +1,47 @@
 import express from "express";
 
-const router = express.Router();
+import conn from "../mariadb.js";
 
-// 리뷰 데이터
-let id = 1;
-let reviews = new Map();
+const router = express.Router();
 
 router
   .route("/")
   // 전체 리뷰 조회: GET "/reviews"
   .get((req, res) => {
-    if (!reviews.size) {
-      return res.status(404).json({ message: "전체 조회할 리뷰가 없습니다." });
-    }
-    let objectReviews = {};
-    reviews.forEach((review, key) => (objectReviews[key] = review));
-    res.status(200).json(objectReviews);
+    let sql = `SELECT * FROM reviews`;
+
+    conn.query(sql, (err, results) => {
+      if (results.length) {
+        res.status(200).json(results);
+      } else {
+        res.status(404).json({ message: "전체 조회할 리뷰가 없습니다." });
+      }
+    });
   })
   // 리뷰 등록: POST "/reviews"
   .post((req, res) => {
-    const { title, description, author, image } = req.body;
+    const { userId, title, description } = req.body;
+    if (!userId) {
+      res.status(400).json({ message: "작성자 아이디를 제대로 보내주세요." });
+      return;
+    }
     if (!title) {
-      res.status(400).json({ message: "상점 이름이 없습니다." });
+      res.status(400).json({ message: "상점 이름을 제대로 보내주세요." });
       return;
     }
     if (!description) {
-      res.status(400).json({ message: "리뷰 컨텐츠가 없습니다." });
+      res.status(400).json({ message: "리뷰 컨텐츠를 제대로 보내주세요." });
       return;
     }
-    if (!author) {
-      res.status(400).json({ message: "작성자가 없습니다." });
-      return;
-    }
-    if (!image) {
-      res.status(400).json({ message: "이미지가 없습니다." });
-      return;
-    }
-    reviews.set(id++, req.body);
-    res.status(200).json(reviews.get(id - 1));
+
+    let sql = `INSERT INTO reviews (user_id, title, description) VALUES(?, ?, ?)`;
+    let values = [userId, title, description];
+    conn.query(sql, values, (err, results) => {
+      if (err) {
+        res.status(500).json({ message: "데이터 베이스 연동 오류입니다." });
+      }
+      res.status(200).json(results);
+    });
   });
 
 router
@@ -45,22 +49,26 @@ router
   // 개별 리뷰 조회: GET "/reviews/:id"
   .get((req, res) => {
     const { id } = req.params;
-    const findReview = reviews.get(Number(id));
-    if (!findReview) {
-      res.status(404).json({ message: "리뷰 데이터가 없습니다." });
-    } else {
-      res.status(200).json(findReview);
-    }
+
+    let sql = `SELECT * FROM reviews WHERE id = ?`;
+
+    conn.query(sql, [id], (err, results) => {
+      if (results.length) {
+        res.status(200).json(results);
+      } else {
+        res.status(404).json({ message: "조회할 리뷰가 없습니다." });
+      }
+    });
   })
   // 리뷰 삭제: DELETE "/reviews/:id"
   .delete((req, res) => {
     const { id } = req.params;
-    const review = reviews.get(Number(id));
-    if (!review) {
-      return res.status(404).json({ message: "삭제할 리뷰가 없습니다." });
-    }
-    reviews.delete(Number(id));
-    res.status(204).json({});
+
+    let sql = `DELETE FROM reviews WHERE id = ?`;
+
+    conn.query(sql, [id], (err, results) => {
+      res.status(200).json(results);
+    });
   });
 
 export default router;
