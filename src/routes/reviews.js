3ffdc595cd05573663904b91a0058fa3b4520bc1
하주영 <ju980304@naver.com>
2024-04-02 22:49:29 +0900

    Feat: 리뷰 관련 API에 유효성 검사 미들웨어 추가
    - express-validator 를 이용하여 요청 값 유효성 검사
    - 요청값이 비었는지, 문자열인지, 숫자인지 검사

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,79 +1,93 @@
 import express from "express";
-
+import { body, param, validationResult } from "express-validator";
 import conn from "../mariadb.js";
 
 const router = express.Router();
 
+const validate = (req, res, next) => {
+  const err = validationResult(req);
+
+  if (!err.isEmpty()) {
+    res.json(err);
+    return;
+  }
+  next();
+};
 router
   .route("/")
   // 전체 리뷰 조회: GET "/reviews"
-  .get((req, res) => {
-    const { userId } = req.body;
-    if (!userId) {
-      res.status(400).json({ message: "작성자 아이디를 제대로 보내주세요." });
-      return;
-    }
-    let sql = `SELECT * FROM reviews WHERE user_id = ?`;
+  .get(
+    [
+      body("userId").notEmpty().isInt().withMessage("Not Integer value"),
+      validate,
+    ],
+    (req, res) => {
+      const { userId } = req.body;
 
-    conn.query(sql, [userId], (err, results) => {
-      if (results.length) {
-        res.status(200).json(results);
-      } else {
-        res.status(404).json({ message: "전체 조회할 리뷰가 없습니다." });
-      }
-    });
-  })
+      let sql = `SELECT * FROM reviews WHERE user_id = ?`;
+
+      conn.query(sql, [userId], (err, results) => {
+        if (results.length) {
+          res.status(200).json(results);
+        } else {
+          res.status(404).json({ message: "전체 조회할 리뷰가 없습니다." });
+        }
+      });
+    }
+  )
   // 리뷰 등록: POST "/reviews"
-  .post((req, res) => {
-    const { userId, title, description } = req.body;
-    if (!userId) {
-      res.status(400).json({ message: "작성자 아이디를 제대로 보내주세요." });
-      return;
-    }
-    if (!title) {
-      res.status(400).json({ message: "상점 이름을 제대로 보내주세요." });
-      return;
-    }
-    if (!description) {
-      res.status(400).json({ message: "리뷰 컨텐츠를 제대로 보내주세요." });
-      return;
-    }
+  .post(
+    [
+      body("userId").notEmpty().isInt().withMessage("Not Integer value"),
+      body("title").notEmpty().isString().withMessage("Not String value"),
+      body("description").notEmpty().isString().withMessage("Not String value"),
+      validate,
+    ],
+    (req, res) => {
+      const { userId, title, description } = req.body;
 
-    let sql = `INSERT INTO reviews (user_id, title, description) VALUES(?, ?, ?)`;
-    let values = [userId, title, description];
-    conn.query(sql, values, (err, results) => {
-      if (err) {
-        res.status(500).json({ message: "데이터 베이스 연동 오류입니다." });
-      }
-      res.status(200).json(results);
-    });
-  });
+      let sql = `INSERT INTO reviews (user_id, title, description) VALUES(?, ?, ?)`;
+      let values = [userId, title, description];
+      conn.query(sql, values, (err, results) => {
+        if (err) {
+          res.status(500).json({ message: "데이터 베이스 연동 오류입니다." });
+        }
+        res.status(200).json(results);
+      });
+    }
+  );
 
 router
   .route("/:id")
   // 개별 리뷰 조회: GET "/reviews/:id"
-  .get((req, res) => {
-    const { id } = req.params;
+  .get(
+    [param("id").notEmpty().isInt().withMessage("Not Integer value"), validate],
+    (req, res) => {
+      const { id } = req.params;
 
-    let sql = `SELECT * FROM reviews WHERE id = ?`;
+      let sql = `SELECT * FROM reviews WHERE id = ?`;
 
-    conn.query(sql, [id], (err, results) => {
-      if (results.length) {
-        res.status(200).json(results);
-      } else {
-        res.status(404).json({ message: "조회할 리뷰가 없습니다." });
-      }
-    });
-  })
+      conn.query(sql, [id], (err, results) => {
+        if (results.length) {
+          res.status(200).json(results);
+        } else {
+          res.status(404).json({ message: "조회할 리뷰가 없습니다." });
+        }
+      });
+    }
+  )
   // 리뷰 삭제: DELETE "/reviews/:id"
-  .delete((req, res) => {
-    const { id } = req.params;
+  .delete(
+    [param("id").notEmpty().isInt().withMessage("Not Integer value"), validate],
+    (req, res) => {
+      const { id } = req.params;
 
-    let sql = `DELETE FROM reviews WHERE id = ?`;
+      let sql = `DELETE FROM reviews WHERE id = ?`;
 
-    conn.query(sql, [id], (err, results) => {
-      res.status(200).json(results);
-    });
-  });
+      conn.query(sql, [id], (err, results) => {
+        res.status(200).json(results);
+      });
+    }
+  );
 
 export default router;
