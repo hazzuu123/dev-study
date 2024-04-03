c0645d2f46435357c99166f98948a5f5d9ec6963
하주영 <ju980304@naver.com>
2024-04-03 22:51:35 +0900

    Feat: express-validator 를 이용하여 유저 관련 API에 유효성 검사 로직 추가
    - validate (유효성 검사 실패 후 로직) 미들웨어를 분리
    - 회원 가입, 개별 유저 조회, 회원 탈퇴 API에 유효성 로직 추가

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,19 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
 
 import conn from "../mariadb.js";
 
 const router = express.Router();
+
+const validate = (req, res, next) => {
+  const err = validationResult(req);
+
+  if (!err.isEmpty()) {
+    res.status(400).json(err);
+    return;
+  }
+  next();
+};
 
 router
   .route("/")
@@ -19,22 +30,38 @@ router
   })
 
   // 회원 가입
-  .post((req, res) => {
-    const { email, password, nickname } = req.body;
-    if (!password || !email || !nickname) {
-      res.status(400).json({ message: "요청값이 부족합니다." });
-      return;
+  .post(
+    [
+      body("email")
+        .notEmpty()
+        .isEmail()
+        .withMessage("이메일 형식으로 보내주세요."),
+      body("password")
+        .notEmpty()
+        .isString()
+        .withMessage("문자열로 보내주세요."),
+      body("nickname")
+        .notEmpty()
+        .isString()
+        .withMessage("문자열로 보내주세요."),
+      validate,
+    ],
+    (req, res) => {
+      const { email, password, nickname } = req.body;
+
+      let sql = `INSERT INTO users (email, password, nickname) VALUES(?, ?, ?)`;
+      let values = [email, password, nickname];
+
+      conn.query(sql, values, (err, results) => {
+        if (err) {
+          return res.status(400).json(err);
+        }
+        res
+          .status(201)
+          .json({ message: `${nickname}님 회원가입이 완료되었습니다.` });
+      });
     }
-
-    let sql = `INSERT INTO users (email, password, nickname) VALUES(?, ?, ?)`;
-    let values = [email, password, nickname];
-
-    conn.query(sql, values, (err, results) => {
-      res
-        .status(201)
-        .json({ message: `${nickname}님 회원가입이 완료되었습니다.` });
-    });
-  })
+  )
 
   // 유저 개별 정보 수정 (PATCH)
   .patch((req, res) => {
@@ -54,39 +81,50 @@ router
   })
 
   // 회원 탈퇴
-  .delete((req, res) => {
-    const { email } = req.body;
+  .delete(
+    [
+      body("email")
+        .notEmpty()
+        .isEmail()
+        .withMessage("이메일 형식으로 보내주세요."),
+      validate,
+    ],
+    (req, res) => {
+      const { email } = req.body;
 
-    if (!email) {
-      res.status(400).json({ message: "요청값이 부족합니다." });
-      return;
+      let sql = `DELETE FROM users WHERE email = ?`;
+      conn.query(sql, [email], (err, results) => {
+        if (results.affectedRows === 0) {
+          return res.status(400).json({ message: "없는 계정입니다." });
+        }
+        res.status(200).json(results);
+      });
     }
-
-    let sql = `DELETE FROM users WHERE email = ?`;
-    conn.query(sql, [email], (err, results) => {
-      res.status(200).json(results);
-    });
-  });
+  );
 
 router
   .route("/detail")
   // 유저 개별 조회
-  .get((req, res) => {
-    const { email } = req.body;
+  .get(
+    [
+      body("email")
+        .notEmpty()
+        .isEmail()
+        .withMessage("이메일 형식으로 보내주세요."),
+      validate,
+    ],
+    (req, res) => {
+      const { email } = req.body;
 
-    if (!email) {
-      res.status(400).json({ message: "요청값이 부족합니다." });
-      return;
+      let sql = `SELECT * FROM users WHERE email = ?`;
+      conn.query(sql, [email], (err, results) => {
+        if (results.length) {
+          res.status(200).json(results[0]);
+        } else {
+          res.status(404).json({ message: "회원 정보가 없습니다." });
+        }
+      });
     }
-
-    let sql = `SELECT * FROM users WHERE email = ?`;
-    conn.query(sql, [email], (err, results) => {
-      if (results.length) {
-        res.status(200).json(results[0]);
-      } else {
-        res.status(404).json({ message: "회원 정보가 없습니다." });
-      }
-    });
-  });
+  );
 
 export default router;
